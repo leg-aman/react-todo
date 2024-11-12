808c37232bd5e6ce1201444ac0447249be270ee7
leg-aman <126026416+leg-aman@users.noreply.github.com>
2024-11-12 07:54:58 -0500

    CTD week two

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-const todoList = [
-  {
-    id: 1,
-    title: "gym",
-  },
-  {
-    id: 2,
-    title: "studying"
-  },
-  {
-    id: 3,
-    title: "practice coding"
-  }
-]
-
+import './TodoList'
+import TodoList from './TodoList'
 function App() {
-  
-
   return (
     <>
       <h1>Todo List</h1>
-      <ul>{
-        todoList.map((i)=>{
-          return <li key={i.id}>{i.title}</li>
-        })
-        }</ul>
+      <TodoList />
     </>
   )
 }
