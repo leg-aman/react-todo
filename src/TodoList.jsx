1aebcfb8a852e3034e346f9e9329262563b49eb2
leg-aman <126026416+leg-aman@users.noreply.github.com>
2024-11-26 15:25:45 -0500

    lesson 1-4 compeleted!

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
-const todoList = [
-    {
-        id: 1,
-        title: "💪 gym",
-    },
-    {
-        id: 2,
-        title: "📖 studying"
-    },
-    {
-        id: 3,
-        title: " 💻practice coding"
-    },
-    {
-        id: 4,
-        title: "🧑‍🤝‍🧑 meeting with mentor"
-    }
-]
-
-const TodoList = () => {
+const TodoList = ({todoList}) => {
     return (
-        <>
-            <ul>
-                {todoList.map((task) => (
-                    <TodoListItem key={task.id} task={task} />
-                ))}
-            </ul>
-        </>
+        <ul>
+            {todoList.map((task) => (
+                <TodoListItem key={task.id} task={task} />
+            ))}
+        </ul>
     );
 };
-
 export default TodoList;
