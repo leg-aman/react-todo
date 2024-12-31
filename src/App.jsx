58287ae055f414a146fc39b872298c74b97bda87
leg-aman <126026416+leg-aman@users.noreply.github.com>
2024-12-31 15:15:21 -0500

    lesson 1-7 | conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,32 @@ import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 import Search from './Search'
 
-
 function App() {
- // Initialize 'todoList' with the value from localStorage or an empty array if not found
+  // Initialize 'todoList' with the value from localStorage or an empty array if not found
   const [todoList, setTodoList] = useState([])
-  const [isLoading, setIsLoading] = useState (true)
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     const myPromise = new Promise((resolve, reject) => {
-      setTimeout( () => {
-        resolve({data : {
-          todoList: JSON.parse(localStorage.getItem('savedTodoList')) || []
-        }})
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem('savedTodoList')) || []
+          }
+        })
       }, 2000)
-      })
-      // extract the todoList from the result object in the .then() method: setTodoList(result.data.todoList)
-      myPromise.then((result)=> {
-        setTodoList(result.data.todoList)
-        setIsLoading(false)
-      
     })
-    }, [])
-  
+    // extract the todoList from the result object in the .then() method: setTodoList(result.data.todoList)
+    myPromise.then((result) => {
+      setTodoList(result.data.todoList)
+      setIsLoading(false)
+
+    })
+  }, [])
+
   // Update localStorage every time 'todoList' changes
   useEffect(() => {
-    if(!isLoading){
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+    if (!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList))
     }
   }, [todoList])  // The hook depends on 'todoList' state change
 
@@ -41,19 +42,19 @@ function App() {
     setTodoList((prevList) => [...prevList, newTodo])  // Update state by appending the new todo
   }
   const removeTodo = (id) => {
-    const newTodoList = todoList.filter(todo => todo.id !== id); 
-    setTodoList(newTodoList); 
+    const newTodoList = todoList.filter(todo => todo.id !== id);
+    setTodoList(newTodoList);
   };
 
   return (
     <>
-    {  isLoading ? <p>Loading...</p> : ''}
-    
+      {isLoading ? <p>Loading...</p> : ''}
+
       <h1>Todo List</h1>
       {/* Pass the addTodo function as a prop to AddTodoForm component */}
       <AddTodoForm onAddTodo={addTodo} />
       {/* Pass the todoList as a prop to the TodoList component */}
-      <TodoList todoList={todoList}  onRemoveTodo={removeTodo}/>
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
       {/* Include the Search component */}
       {/* <Search /> */}
     </>
